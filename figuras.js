213a78a0efdc36c6c5fcee0318a7b66470d525e5
wmorales <wmorales@kubixcorp.com>
2022-05-26 16:58:03 -0600

    Se agregó el codigo de triangulo y se agruparon las funciones

--- figuras.js
+++ figuras.js
@@ -1,5 +1,7 @@
 //codigo del cuadrado
 
+console.group("Cuadrados");
+
 const ladoCuadrado = 5;
 console.log("Los Lados del Cuadrado miden:" + ladoCuadrado+"cm");
 
@@ -8,3 +10,36 @@ console.log("El Perimetro del cuadrado es:" + perimetroCuadrado+"cm");
 
 const areaCuadrado = ladoCuadrado * ladoCuadrado;
 console.log("El Area cuadrado es:" + areaCuadrado+"cm2");
+
+console.groupEnd;
+
+
+
+console.group("Triangulos");
+//codigo del triangulo
+
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const baseTriangulo = 4;
+
+console.log(
+    "Los Lados del triángulo miden:" 
+    + ladoTriangulo1 
+    + "cm, "
+    + ladoTriangulo2 
+    + "cm, "
+    + baseTriangulo
+    + "cm"
+);
+const alturaTriangulo = 5.5;
+console.log("La Altura del triangulo es de: " + alturaTriangulo);
+
+const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 +baseTriangulo;
+
+console.log( "El perimetro del triangulo es de:" + perimetroTriangulo+"cm")
+
+const areaTriangulo = (baseTriangulo * alturaTriangulo ) / 2;
+
+console.log ("El Área del triangulo es de:" + areaTriangulo + "cm2")
+
+console.groupEnd;
