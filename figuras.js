34c28cf1550892c8c064509e07bd0e25bb749236
wmorales <wmorales@kubixcorp.com>
2022-05-26 16:29:25 -0600

    codigo cuadrado

--- figuras.js
+++ figuras.js
@@ -1,1 +1,10 @@
-console.log("hola mundo")
+//codigo del cuadrado
+
+const ladoCuadrado = 5;
+console.log("Los Lados del Cuadrado miden:" + ladoCuadrado+"cm");
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("El Perimetro del cuadrado es:" + perimetroCuadrado+"cm");
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log("El Area cuadrado es:" + areaCuadrado+"cm2");
