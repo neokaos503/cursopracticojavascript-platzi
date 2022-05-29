f3e86f4e4fd3cab5bee6c726c8e188513c044390
wmorales <wmorales@kubixcorp.com>
2022-05-29 17:27:24 -0600

    se agrego formula para  altura de  triangulo hipotenusa etc

--- figuras.js
+++ figuras.js
@@ -51,6 +51,23 @@ function areaTriangulo (base, altura) {
 
 //console.log ("El Área del triangulo es de:" + areaTriangulo + "cm2")
 
+function alturaTrianguloIsosceles(trianguloGrandeLadoA, trianguloGrandeLadoB, trianguloGrandeLadoBase) {
+    if (trianguloGrandeLadoA != trianguloGrandeLadoB) {
+        console.error("Los lados a y b no son iguales");
+    } else {
+        const trianguloPequenoLadoB = trianguloGrandeLadoBase / 2;
+        const trianguloPequenoLadoBase = trianguloGrandeLadoA;
+
+        const trianguloPequenoLadoBCuadrado = trianguloPequenoLadoB * trianguloPequenoLadoB;
+        const trianguloPequenoLadoBaseCuadrado = trianguloPequenoLadoBase * trianguloPequenoLadoBase;
+
+        const trianguloPequenoLadoA = Math.sqrt(trianguloPequenoLadoBaseCuadrado - trianguloPequenoLadoBCuadrado);
+
+        const trianguloGrandeAltura = trianguloPequenoLadoA;
+        return trianguloGrandeAltura;
+    }
+}
+
 console.groupEnd;
 
 console.group("Circulos");
