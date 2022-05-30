9db1d4261d4417e2aaab0dfa8d84dd23976dc915
wmorales <wmorales@kubixcorp.com>
2022-05-29 22:22:57 -0600

    se agrego funcion para  sacar  descuentos

--- descuentos.js
+++ descuentos.js
@@ -1,16 +1,29 @@
-const precioOriginal =100;
-const descuento=15;
+const precioOriginal = 100;
+const descuento = 15;
 
-
-
-function calcularPrecioConDescuento(precio,descuento){
-    const porcentajePrecioConDescuento = 100 - descuento;
-    const precioConDescuento = ((precio * porcentajePrecioConDescuento)/100);
-return precioConDescuento
+function calcularPrecioConDescuento(precio, descuento) {
+  const porcentajePrecioConDescuento = 100 - descuento;
+  const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+  return precioConDescuento;
 }
 //console.log ({
-  //  precioOriginal,
-    //descuento,
-    //porcentajePrecioConDescuento,
-    //precioConDescuento,
+//  precioOriginal,
+//descuento,
+//porcentajePrecioConDescuento,
+//precioConDescuento,
 //});
+
+function onClickButtonPriceDiscount() {
+  const inputPrice = document.getElementById("inputPrice");
+  const priceValue = inputPrice.value;
+  const inputDiscount = document.getElementById("inputDiscount");
+  const discountValue = inputDiscount.value;
+
+  const precioConDescuento = calcularPrecioConDescuento(
+    priceValue,
+    discountValue
+  );
+
+  const resultPrice = document.getElementById("resultPrice");
+  resultPrice.innerText = "El precio con descuento es: $" + precioConDescuento;
+}
